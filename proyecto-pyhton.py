2f50b18cbf584b18f1d1b69dbddcab9948c669c3
JoaquinCancino <130596278+JoaquinCancino@users.noreply.github.com>
2024-05-23 14:35:02 -0400

    Create proyecto-pyhton.py

---
+++ proyecto-pyhton.py
@@ -0,0 +1,10 @@
+def hola_mundo():
+    print ('hola mundo')
+
+hola_mundo()
+
+
+def modificacion1()):
+    print ('esta es una segunda modificacion al software, que subiremos a GitHub')
+
+modificacion1()
