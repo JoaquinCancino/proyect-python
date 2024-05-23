f564339ef6c33f7f760ff07d2821cc2cf5580e41
JoaquinCancino <130596278+JoaquinCancino@users.noreply.github.com>
2024-05-23 14:36:25 -0400

    Update proyecto-pyhton.py

--- proyecto-pyhton.py
+++ proyecto-pyhton.py
@@ -8,3 +8,11 @@ def modificacion1()):
     print ('esta es una segunda modificacion al software, que subiremos a GitHub')
 
 modificacion1()
+
+
+
+
+def modificacion_ramas()):
+    print ('esta es una segunda modificacion al software con ramas, que subiremos a GitHub')
+
+modificacion_ramas()
